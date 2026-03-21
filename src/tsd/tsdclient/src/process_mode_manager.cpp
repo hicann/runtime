aede7748e58715e9340a84ddb57787ac8dcaf56e
pan-tong <pantong@huawei.com>
2026-03-21 14:56:33 +0800

    910B不支持cann-hcomm-compat.tar.gz和cann-tsch-compat.tar.gz插件包加载
    Co-authored-by: pan-tong<pantong@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1008 merge cherry-pick-mr-1007-1773885654142-auto into 9.0.0
    
    910B不支持cann-hcomm-compat.tar.gz和cann-tsch-compat.tar.gz插件包加载
    
    Created-by: pan-tong
    Commit-by: pan-tong
    Merged-by: cann-robot
    Description: ## 描述
    910B不支持cann-hcomm-compat.tar.gz和cann-tsch-compat.tar.gz插件包加载
    
    ## 关联的Issue
    https://gitcode.com/cann/runtime/issues/200
    
    ## 测试
    执行tsdopen用例，确认910B不加载cann-hcomm-compat.tar.gz和cann-tsch-compat.tar.gz插件包
    ![image.png](https://raw.gitcode.com/user-images/assets/8810053/061df3ee-2db8-4063-b587-e95a081d8627/image.png 'image.png')
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：方案调整
    
    
    
    
    See merge request: cann/runtime!1008

--- src/tsd/tsdclient/src/process_mode_manager.cpp
+++ src/tsd/tsdclient/src/process_mode_manager.cpp
@@ -66,9 +66,9 @@ constexpr uint32_t SOC_VERSION_LEN = 50U;
 const std::map<std::string, std::vector<tsd::ChipType_t>> PKG_CHIP_SUPPORT_MAP = {
     {"aicpu_hccl.tar.gz", {tsd::CHIP_ASCEND_910B, tsd::CHIP_ASCEND_950, tsd::CHIP_CLOUD_V5}},
     {"aicpu_hcomm.tar.gz", {tsd::CHIP_DC, tsd::CHIP_ASCEND_910B, tsd::CHIP_ASCEND_950, tsd::CHIP_CLOUD_V5}},
-    {"cann-hcomm-compat.tar.gz", {tsd::CHIP_ASCEND_910B, tsd::CHIP_ASCEND_950, tsd::CHIP_CLOUD_V5}},
+    {"cann-hcomm-compat.tar.gz", {tsd::CHIP_ASCEND_950, tsd::CHIP_CLOUD_V5}},
     {HCCD_PKG_NAME, {tsd::CHIP_ASCEND_910B}},
-    {"cann-tsch-compat.tar.gz", {tsd::CHIP_ASCEND_910B}},
+    {"cann-tsch-compat.tar.gz", {}},
     {UDF_PKG_NAME, {tsd::CHIP_ASCEND_910B}},
     {HIXL_PKG_NAME, {tsd::CHIP_ASCEND_910B, tsd::CHIP_ASCEND_950, tsd::CHIP_CLOUD_V5}}
 };
