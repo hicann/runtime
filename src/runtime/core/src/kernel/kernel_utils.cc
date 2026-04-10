61933c49904e6cdacb05ddce376c202c43ca6913
xdurainbow <liudongmiao@huawei.com>
2026-04-10 14:45:35 +0800

    fix errMsg print format
    Co-authored-by: Mark<liudongmiao@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1504 merge master into master
    
    fix errMsg print format
    
    Created-by: xdurainbow
    Commit-by: Mark
    Merged-by: cann-robot
    Description: ## 描述
    修复ErrMsg打印时传入的参数格式：
    EE1001后面只跟随一个extend字符串，不添加func
    
    ## 关联的Issue
    https://gitcode.com/cann/runtime/issues/352
    
    ## 测试
    修改后可以正常打印出对应的ErrMsg
    
    ## 文档更新
    bugfix，不涉及文档更新
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/runtime!1504

--- src/runtime/core/src/kernel/kernel_utils.cc
+++ src/runtime/core/src/kernel/kernel_utils.cc
@@ -188,18 +188,18 @@ rtError_t UpdateKernelParams(TaskInfo* const taskInfo, rtTaskParams* const param
 {
     COND_RETURN_AND_MSG_OUTER(
         (taskInfo->type != TS_TASK_TYPE_KERNEL_AICORE && taskInfo->type != TS_TASK_TYPE_KERNEL_AIVEC),
-        RT_ERROR_INVALID_VALUE, ErrorCode::EE1001, __func__, "now only support update kernel to kernel.");
+        RT_ERROR_INVALID_VALUE, ErrorCode::EE1001, "now only support update kernel to kernel");
     COND_RETURN_AND_MSG_OUTER((params->taskGrp != nullptr),
-        RT_ERROR_INVALID_VALUE, ErrorCode::EE1001, __func__, "taskGrp must be nullptr.");
+        RT_ERROR_INVALID_VALUE, ErrorCode::EE1001, "taskGrp must be nullptr");
 
     const rtKernelTaskParams* kernelTaskParams = &(params->kernelTaskParams);
     Kernel* const kernel = RtPtrToPtr<Kernel*>(kernelTaskParams->funcHandle);
     COND_RETURN_AND_MSG_OUTER((kernel == nullptr),
-        RT_ERROR_INVALID_VALUE, ErrorCode::EE1001, __func__, "funcHandle cannot be nullptr.");
+        RT_ERROR_INVALID_VALUE, ErrorCode::EE1001, "funcHandle cannot be nullptr");
     COND_RETURN_AND_MSG_OUTER((kernelTaskParams->args == nullptr),
-        RT_ERROR_INVALID_VALUE, ErrorCode::EE1001, __func__, "args cannot be nullptr.");
+        RT_ERROR_INVALID_VALUE, ErrorCode::EE1001, "args cannot be nullptr");
     COND_RETURN_AND_MSG_OUTER((kernelTaskParams->argsSize == 0U),
-        RT_ERROR_INVALID_VALUE, ErrorCode::EE1001, __func__, "argsSize cannot be 0.");
+        RT_ERROR_INVALID_VALUE, ErrorCode::EE1001, "argsSize cannot be 0");
 
     TaskCfg taskCfg = {};
     rtError_t error = ConvertLaunchCfgToTaskCfg(taskCfg, kernelTaskParams->cfg);
