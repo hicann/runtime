8b11ff3cce003251950c24f61fd20460bf3cda80
Larry_liuyi <larry.liuyi@huawei.com>
2026-07-09 20:35:22 +0800

    【PR】: 简要描述，修复gcc 7下编译异常问题
    Co-authored-by: liuyi<larry.liuyi@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3549 merge master into master
    
    【PR】: 简要描述，修复gcc 7下编译异常问题
    
    Created-by: Larry_liuyi
    Commit-by: liuyi
    Merged-by: cann-robot
    Description: 
    # Pull Request
    
    ## 描述
    请清晰准确地描述本次 Pull Request 的意图和变更内容。
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [ ] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1.
    2.
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [ ] 我的代码遵循了项目的代码风格
    - [ ] 我已对代码进行了自测
    - [ ] 我已更新了相关的文档
    - [ ] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [ ] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    See merge request: cann/runtime!3549

--- .gitcode/workflows/arm_compile.sh
+++ .gitcode/workflows/arm_compile.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
 
 echo $(grep -E "^VERSION_ID=" /etc/os-release | cut -d'"' -f2)
 if [[ "${task_name}" == *ubuntu24* ]]; then
     sudo update-alternatives --set gcc /usr/bin/gcc-14
 else
+    rm -rf /home/jenkins/opensource/lib_cache
+    ln -s /home/jenkins/opensource/ubuntu20/lib_cache /home/jenkins/opensource/lib_cache
     if [[ -f "/opt/rh/devtoolset-7/enable" ]]; then
         echo "source devtoolset"
         source /opt/rh/devtoolset-7/enable
