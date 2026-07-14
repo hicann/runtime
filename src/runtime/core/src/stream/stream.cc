2a8a94bbc09bdd4c24fadeb1de1dbda7d522ba95
Rick_Coding <yangsen24@huawei.com>
2026-07-14 11:16:45 +0800

    【PR】: fix: 删除C头文件inttypes.h，改为使用C++头文件cinttypes
    Co-authored-by: Rick_Coding<yangsen24@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3626 merge master into master
    
    【PR】: fix: 删除C头文件inttypes.h，改为使用C++头文件cinttypes
    
    Created-by: Rick_Coding
    Commit-by: Rick_Coding
    Merged-by: cann-robot
    Description: 
    # Pull Request
    
    ## 描述
    删除C头文件inttypes.h，改为使用C++头文件cinttypes
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [ ] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1.通过A5 RDV测试
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [ ] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    See merge request: cann/runtime!3626

--- src/runtime/core/src/stream/stream.cc
+++ src/runtime/core/src/stream/stream.cc
@@ -7,7 +7,7 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
-#include <inttypes.h>
+#include <cinttypes>
 #include <iomanip>
 #include "stream.hpp"
 #include "model_update_task.h"
--- src/runtime/core/src/task/task_info/dump/dump_task.cc
+++ src/runtime/core/src/task/task_info/dump/dump_task.cc
@@ -8,7 +8,7 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#include <inttypes.h>
+#include <cinttypes>
 #include "stream.hpp"
 #include "runtime.hpp"
 #include "context.hpp"
