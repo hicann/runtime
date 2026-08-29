8c1621929e16a789fd74c0c327021c31e6a9828c
tanghaoran7 <tanghaoran7@huawei.com>
2026-08-29 14:21:11 +0800

    流水线gcc15适配
    Co-authored-by: tanghaoran7<tanghaoran7@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4587 merge master into master
    
    流水线gcc15适配
    
    Created-by: tanghaoran7
    Commit-by: tanghaoran7
    Merged-by: cann-robot
    Description: 
    # Pull Request
    
    ## 描述
    请清晰准确地描述本次 Pull Request 的意图和变更内容。
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [ ] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1.
    2.
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [ ] 我的代码遵循了项目的代码风格
    - [ ] 我已对代码进行了自测
    - [ ] 我已更新了相关的文档
    - [ ] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [ ] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    See merge request: cann/runtime!4587

--- .gitcode/scripts/compile.sh
+++ .gitcode/scripts/compile.sh
@@ -36,19 +36,40 @@ fi
 
 echo $(grep -E "^VERSION_ID=" /etc/os-release | cut -d'"' -f2)
 if [[ "${task_name}" == *ubuntu24* ]]; then
-    sudo update-alternatives --set gcc /usr/bin/gcc-14
+    if [[ "${GIT_TARGET_BRANCH}" = "master" ]]; then
+        sudo update-alternatives --set gcc /usr/bin/gcc-15
+    else
+        sudo update-alternatives --set gcc /usr/bin/gcc-14
+    fi
     sed -i "1i set(CMAKE_EXPORT_COMPILE_COMMANDS ON)" "CMakeLists.txt"
 else
     if [[ -f "/opt/rh/devtoolset-7/enable" ]]; then
-        rm -rf /home/jenkins/opensource/lib_cache
-        ln -s /home/jenkins/opensource/ubuntu20/lib_cache /home/jenkins/opensource/lib_cache
         echo "source devtoolset"
         source /opt/rh/devtoolset-7/enable
     fi
 fi
 
+if gcc --version | head -n1 | grep -q "15\."; then
+    gcc --version
+    rm -rf /home/jenkins/opensource/lib_cache
+    if [ -d  /home/jenkins/opensource/gcc15 ];then
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15/lib_cache /home/jenkins/opensource/lib_cache
+    elif [ -d  /home/jenkins/opensource/gcc15x86 ];then
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15x86/lib_cache /home/jenkins/opensource/lib_cache
+    fi
+elif gcc --version | head -n1 | grep -q "14\."; then
+    gcc --version
+else
+    gcc --version
+    rm -rf /home/jenkins/opensource/lib_cache
+    ln -s /home/jenkins/opensource/ubuntu20/lib_cache /home/jenkins/opensource/lib_cache
+fi
+
 set +e
-gcc --version
 rm -rf /home/jenkins/opensource/json
 source /home/jenkins/Ascend/cann/bin/setenv.bash
 LOG_DO bash build.sh --cann_3rd_lib_path="/home/jenkins/opensource" -f "pr_filelist.txt"
--- .gitcode/scripts/ut.sh
+++ .gitcode/scripts/ut.sh
@@ -22,9 +22,33 @@ function LOG_DO() {
 }
 
 echo $(grep -E "^VERSION_ID=" /etc/os-release | cut -d'"' -f2)
-sudo update-alternatives --set gcc /usr/bin/gcc-14
+if [[ "${TARGET_BRANCH}" = "master" ]]; then
+    sudo update-alternatives --set gcc /usr/bin/gcc-15
+else
+    sudo update-alternatives --set gcc /usr/bin/gcc-14
+fi
+
+if gcc --version | head -n1 | grep -q "15\."; then
+    gcc --version
+    rm -rf /home/jenkins/opensource/lib_cache
+    if [ -d  /home/jenkins/opensource/gcc15 ];then
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15/lib_cache /home/jenkins/opensource/lib_cache
+    elif [ -d  /home/jenkins/opensource/gcc15x86 ];then
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15x86/lib_cache /home/jenkins/opensource/lib_cache
+    fi
+elif gcc --version | head -n1 | grep -q "14\."; then
+    gcc --version
+else
+    gcc --version
+    rm -rf /home/jenkins/opensource/lib_cache
+    ln -s /home/jenkins/opensource/ubuntu20/lib_cache /home/jenkins/opensource/lib_cache
+fi
+
 set +e
-gcc --version
 rm -rf /home/jenkins/opensource/json
 source /home/jenkins/Ascend/cann/bin/setenv.bash
 if [ "${TARGET_BRANCH}" = "master" ];then
